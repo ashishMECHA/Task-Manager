2852213622db4b81405f4187aac8012de0db4b34
Ashish Thakur <thakurashish525@gmail.com>
2022-08-22 20:37:12 +0530

    Added my project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+const port = 4000
+
+// middleware
+
+app.use(express.json())
+
+app.get('/hello', (req,res)=>{
+    res.send('task manager app')
+})
+
+app.use('/api/v1/tasks',tasks)
+
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
