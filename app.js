3bdbff62ed144fb0b4a5c7a32204f550e5c996df
Ashish Thakur <thakurashish525@gmail.com>
2022-11-05 18:35:18 +0530

    new files added

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+require('./db/connect')
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+
+
 
 const port = 4000
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
-app.get('/hello', (req,res)=>{
-    res.send('task manager app')
-})
+
 
 app.use('/api/v1/tasks',tasks)
 
+app.use(notFound)
 
-app.listen(port, console.log(`Server is listening on port ${port}...`))
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
+
+
