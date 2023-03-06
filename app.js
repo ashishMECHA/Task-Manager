a3f8ed8ed51e8939655e266f3d422439ee1f684f
Ashish Thakur <thakurashish525@gmail.com>
2023-03-06 19:39:33 +0530

    middlewares added

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error')
 
 
 
-const port = 4000
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.static('./public'))
@@ -17,7 +18,7 @@ app.use(express.json())
 
 
 app.use('/api/v1/tasks',tasks)
-
+app.use(errorHandlerMiddleware)
 app.use(notFound)
 
 const start = async () =>{
